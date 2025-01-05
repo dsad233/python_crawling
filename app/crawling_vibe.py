606f9b70ac79f7d7cbb8538fce0a3bd90ef7e3d0
dsad233 <ksl1421@naver.com>
2025-01-05 22:42:14 +0900

    Fix : vibe selenium 크롤링 테스트

--- app/crawling_vibe.py
+++ app/crawling_vibe.py
@@ -1,19 +1,45 @@
-import requests
-from bs4 import BeautifulSoup
+# selenium의 webdriver를 사용하기 위한 import
+from selenium import webdriver
+
+# selenium으로 키를 조작하기 위한 import
+# from selenium.webdriver.common.keys import Keys
+
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
+# import time
 import json
 
-headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get('https://vibe.naver.com/chart/domestic/',headers=headers)
- 
-soup = BeautifulSoup(data.text, 'html.parser')
+driver = webdriver.Chrome()
+driver.implicitly_wait(time_to_wait=10)
 
-bestMusicList = soup.find("body")
 
-print(bestMusicList)
-# for i in bestMusicList:
-#     # title = i.select("td.song > div.title_badge_wrap > span > a")
-#     print(bestMusicList)
+driver.get('https://vibe.naver.com/chart/domestic/')
 
-#     if(len(bestMusicList) == 100):
-#         break
+rankArray = []
+titleArray = []
+saveArray = []
+try :
+    # search = driver.find_elements(By.CSS_SELECTOR, "#content > div.track_section > div:nth-child(2) > div > table > tbody")
+    # for data in search:
+    rank = driver.find_elements(By.CSS_SELECTOR, "#content > div.track_section > div:nth-child(2) > div > table > tbody > tr > td > span.text")
+    for data in rank:
+        if(data.text != ""):
+            rankArray.append(data.text)
 
+    title = driver.find_elements(By.CSS_SELECTOR, "#content > div.track_section > div:nth-child(2) > div > table > tbody > tr > td.song > div > span > a")
+    for data in title:
+        if(data.text != ""):
+            titleArray.append(data.text)
+    
+    for j, k in zip(rankArray, titleArray):
+        saveArray.append({ "rank" : j, "title" : k })
+
+    
+
+    print(json.dumps(saveArray, indent=4, ensure_ascii=False))
+except Exception as err:
+    print(err)
+    print("에러 발생")
+finally:
+    driver.close()
